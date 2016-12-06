958f713ff83e248f432880629ec9ac60aca25f89
saracouto1318 <saracouto1318@hotmail.com>
2016-12-06 15:31:39 +0000

    Restaurant oage almost done

--- templates/restaurant_info.php
+++ templates/restaurant_info.php
@@ -1,6 +1,15 @@
-<div id="restaurantBackground">
-	<div id="restaurantNameScore">
-		<h3 class="restaurantName">Restaurant Name </h3>
-		<h3 class="avg">8</h3>
+<div id="restaurantDiv">
+	<div id="restaurantBackground">
+		<div id="restaurantNameScore">
+			<h3 class="restaurantName">Restaurant Name </h3>
+			<h3 class="avg">8</h3>
+		</div>
 	</div>
+	<div id="restauranteInfo">
+		<ul id ="restaurant">
+			<li>Overwiew</li>
+			<li>Menu</li>
+			<li>Photos</li>
+			<li>Review</li>
+		</ul>
 </div>
--- templates/tabs.php
+++ templates/tabs.php
@@ -1,5 +1,5 @@
 <div id="tab">
-	<script src="../data.js"></script>
+	<script src="data.js"></script>
 	<input type="checkbox" onclick="handleChecks(this)" checked="" name="Hello" value="">Hi<br>
 	<input class="button" type="button" value="Top 5" onclick="getTopRestaurants(5)"/>
 
